0ad85855bbe1271988a2fd8b8b54bf02ae3da620
Samandar <samandar1031github@gmail.com>
2021-01-06 10:02:17 +0500

    ; add

--- the-hurdle-race.js
+++ the-hurdle-race.js
@@ -5,8 +5,8 @@
 function hurdleRace(k, height) {
    let max = Math.max(...height);
    if (k > max) {
-      return 0
+      return 0;
    } else {
-      return max - k
+      return max - k;
    }
 }
