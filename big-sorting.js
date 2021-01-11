edcb6ad76a85a94c0b4d7397123e8d691b652080
Samandar <samandar1031github@gmail.com>
2021-01-11 08:10:15 +0500

    +1 challange add

---
+++ big-sorting.js
@@ -0,0 +1,13 @@
+/*
+   Big Sorting
+*/
+
+function bigSorting(unsorted) {
+   unsorted.sort(function (a, b) {
+      if (a.length == b.length) {
+         return a > b ? 1 : -1;
+      }
+      return a.length - b.length
+   })
+   return unsorted;
+}
