18d9a0564d355abcfaf9a5950cf2ac56e9209c68
Samandar <samandar1031github@gmail.com>
2021-01-13 10:37:10 +0500

    calculate sum add

--- filter.js
+++ filter.js
@@ -11,7 +11,7 @@ function filterAct(arr) {
    })
 }
 
-function findMin() {
+function findMin(arr) {
    let min = arr[0];
    arr.forEach(function (elem) {
       if (elem < min) {
@@ -19,4 +19,16 @@ function findMin() {
       }
    });
 }
+
+function calculateSum(arr) {
+   let sum = 0;
+   arr.forEach(function (elem, indx) {
+      if (indx % 2 === 1) {
+         for (i = 0; i < arr.length; i++) {
+            sum += elem;
+         }
+      }
+   })
+}
+
 filterAct([12, 45, 76, 80, 103, 10, 7]);
