a9d94674c8d10fc2c528e6a5ed053c6158d398cf
Samandar <samandar1031github@gmail.com>
2021-02-04 12:12:32 +0500

    updated code

--- map.js
+++ map.js
@@ -1,8 +1,10 @@
 // task 1
 
 let text = "Five little ducks swimming out one day, over the hills and far away";
-let splittedText = text.split(" ");
-let upperCasedText = splittedText.map(function (elem) {
-   return elem[0].toUpperCase() + elem.slice(1);
-});
-upperCasedText.join(" ");
+function uppercaseTxt(str) {
+   let splittedText = str.split(" ");
+   let upperCasedText = splittedText.map(function (elem) {
+      return elem[0].toUpperCase() + elem.slice(1);
+   });
+   return upperCasedText.join(" ");
+}
